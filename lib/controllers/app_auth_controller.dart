ae2145868e3b950afaf48ae96b05d1b31279bade
Firkat <57084614+firkatdavletov@users.noreply.github.com>
2022-12-04 11:10:04 +0500

    Create refresh token method

--- lib/controllers/app_auth_controller.dart
+++ lib/controllers/app_auth_controller.dart
@@ -2,6 +2,7 @@ import 'dart:io';
 
 import 'package:auth/models/response_model.dart';
 import 'package:auth/models/user.dart';
+import 'package:auth/utils/app_utils.dart';
 import 'package:conduit/conduit.dart';
 import 'package:jaguar_jwt/jaguar_jwt.dart';
 
@@ -79,32 +80,20 @@ class AppAuthController extends ResourceController {
     }
   }
 
-  Future<void> _updateTokens(int id, ManagedContext transaction) async {
-    final Map<String, dynamic> tokens = _getTokens(id);
-
-    final qUpdateTokens = Query<User>(transaction)
-      ..where((user) => user.id).equalTo(id)
-      ..values.accessToken = tokens["access"]
-      ..values.refreshToken = tokens["refresh"];
-    await qUpdateTokens.updateOne();
-  }
-
   @Operation.post("refresh")
   Future<Response> refreshToken(
       @Bind.path("refresh") String refreshToken) async {
-    final User fetchedUser = User();
-
-    //connect to DB
-    //find User
-    //check token
-    //fetch user
-
-    return Response.ok(AppResponseModel(data: {
-      "id": fetchedUser.id,
-      "refreshToken": fetchedUser.refreshToken,
-      "accessToken": fetchedUser.accessToken
-    }, message: "Успешное обновление токенов")
-        .toJson());
+    try {
+      final id = AppUtils.getIdFromToken(refreshToken);
+      await _updateTokens(id, managedContext);
+      final user = await managedContext.fetchObjectWithID<User>(id);
+      return Response.ok(AppResponseModel(
+          data: user?.backing.contents,
+          message: "Успешное обновление токенов"));
+    } catch (error) {
+      return Response.serverError(
+          body: AppResponseModel(message: error.toString()));
+    }
   }
 
   Map<String, dynamic> _getTokens(int id) {
@@ -122,5 +111,15 @@ class AppAuthController extends ResourceController {
     tokens["refresh"] = issueJwtHS256(refreshClaimSet, key);
 
     return tokens;
+  }
+
+  Future<void> _updateTokens(int id, ManagedContext transaction) async {
+    final Map<String, dynamic> tokens = _getTokens(id);
+
+    final qUpdateTokens = Query<User>(transaction)
+      ..where((user) => user.id).equalTo(id)
+      ..values.accessToken = tokens["access"]
+      ..values.refreshToken = tokens["refresh"];
+    await qUpdateTokens.updateOne();
   }
 }
